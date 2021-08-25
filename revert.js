ef039399779ba06e87554757a6728604adb96286
Nathan Valentin <nathan.valentin@laposte.net>
2021-08-25 17:33:55 +0200

    it works

--- revert.js
+++ revert.js
@@ -1,3 +1,11 @@
-const revert = (str) => str + str
+const revert = (str) => {
+    let word = str.split('')
+    let reverted = "";
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverted += word[i];
+    }
+    return reverted;
+}
 
 module.exports = revert
